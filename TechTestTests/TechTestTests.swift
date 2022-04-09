6079e528a6baff36ad064eab23574c19e867d006
Xavier Serra <nesxss@gmail.com>
2022-04-09 08:29:33 +0200

    Test base First test for first use case

--- TechTestTests/TechTestTests.swift
+++ TechTestTests/TechTestTests.swift
@@ -1,36 +1,43 @@
-//
-//  TechTestTests.swift
-//  TechTestTests
-//
-//  Created by Xavier Serra Soteras on 2/4/22.
-//
-
 import XCTest
+import Combine
+import SwiftyMocky
+
 @testable import TechTest
 
-class TechTestTests: XCTestCase {
+class GetMarvelEntityTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var mockApiClient: APIClientMock!
+    var getMarvelEntity: GetMarvelEntityUseCase!
+    let anyCharacter = Marvel.MarvelEntity(id: 1, name: "name", description: "description", source: .character, thumbnailURL: nil, wikiURL: nil, detailURL: nil)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func setUp() {
+        super.setUp()
+        mockApiClient = APIClientMock()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        CoreServiceLocator.shared.add {
+            Register(APIClient.self) { self.mockApiClient }
         }
+
+        getMarvelEntity = GetMarvelEntity()
     }
 
+    override func tearDown() {
+        mockApiClient = nil
+        getMarvelEntity = nil
+
+        super.tearDown()
+    }
+
+    func testFetchesCharacters() throws {
+        givenApiClientGetCharacterSucceeds()
+        try expect(getMarvelEntity(type: .character), toPublish: [anyCharacter])
+    }
+
+    //MARK: Given
+
+    private func givenApiClientGetCharacterSucceeds() {
+        Given(mockApiClient, .getCharacters(
+            willReturn: .success(value: [anyCharacter])
+        ))
+    }
 }
