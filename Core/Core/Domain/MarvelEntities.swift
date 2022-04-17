c9850b4f16ebab608586c1ceea5b49bf1aecc48c
Xavier Serra <nesxss@gmail.com>
2022-04-17 18:45:33 +0200

    List section and category

--- Core/Core/Domain/MarvelEntities.swift
+++ Core/Core/Domain/MarvelEntities.swift
@@ -13,28 +13,22 @@ public enum Marvel {
     public struct MarvelEntity: Equatable {
         public let id: Int
         public let name: String
-        public let description: String
+        public let description: String?
         public let source: Source
         public let thumbnailURL: URL?
-        public let wikiURL: URL?
-        public let detailURL: URL?
 
         public init(
             id: Int,
             name: String,
-            description: String,
+            description: String?,
             source: Source,
-            thumbnailURL: URL?,
-            wikiURL: URL?,
-            detailURL: URL?
+            thumbnailURL: URL?
         ) {
             self.id = id
             self.name = name
             self.description = description
             self.source = source
             self.thumbnailURL = thumbnailURL
-            self.wikiURL = wikiURL
-            self.detailURL = detailURL
         }
     }
 }
--- TechTest/MarvelList/View/MarvelListSection.swift
+++ TechTest/MarvelList/View/MarvelListSection.swift
@@ -1,21 +1,56 @@
 import Core
 
 class Section: Hashable {
-    enum `Type`: String {
+    enum Category: CaseIterable {
+        case all
         case characters
         case comics
         case creators
         case events
         case series
+
+        init?(source: Marvel.Source) {
+            switch source {
+            case .character:
+                self = .characters
+            case .comics:
+                self = .comics
+            case .creators:
+                self = .creators
+            case .events:
+                self = .events
+            case .series:
+                self = .series
+            case .stories:
+                return nil
+            }
+        }
+
+        var localizedTitle: String {
+            switch self {
+            case .all:
+                return L10n.listSectionTitleAll
+            case .characters:
+                return L10n.listSectionTitleCharacters
+            case .comics:
+                return L10n.listSectionTitleComics
+            case .creators:
+                return L10n.listSectionTitleCreators
+            case .events:
+                return L10n.listSectionTitleEvents
+            case .series:
+                return L10n.listSectionTitleSeries
+            }
+        }
     }
 
     let id: String
     let title: String
     let models: [MarvelListCollectionModel]
 
-    init(type: `Type`, models: [MarvelListCollectionModel]) {
-        self.id = type.rawValue
-        self.title = type.rawValue.capitalized
+    init(category: Category, models: [MarvelListCollectionModel]) {
+        self.id = String(category.hashValue)
+        self.title = category.localizedTitle
         self.models = models
     }
 
@@ -27,4 +62,3 @@ class Section: Hashable {
         lhs.id == rhs.id
     }
 }
-
