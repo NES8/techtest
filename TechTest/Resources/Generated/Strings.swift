8e6f1c4b5af5ab65c67188d4c4eba2f33a5af1a2
Xavier Serra <nesxss@gmail.com>
2022-04-17 18:48:18 +0200

    Translations

--- TechTest/Resources/Generated/Strings.swift
+++ TechTest/Resources/Generated/Strings.swift
@@ -10,8 +10,32 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  /// Filter
+  internal static let listFilterMenuTitle = L10n.tr("Localizable", "List FilterMenu Title")
+  /// Name starts with...
+  internal static let listSearchPlaceholder = L10n.tr("Localizable", "List Search Placeholder")
+  /// All
+  internal static let listSectionTitleAll = L10n.tr("Localizable", "List SectionTitle all")
+  /// Characters
+  internal static let listSectionTitleCharacters = L10n.tr("Localizable", "List SectionTitle characters")
+  /// Comics
+  internal static let listSectionTitleComics = L10n.tr("Localizable", "List SectionTitle comics")
+  /// Creators
+  internal static let listSectionTitleCreators = L10n.tr("Localizable", "List SectionTitle creators")
+  /// Events
+  internal static let listSectionTitleEvents = L10n.tr("Localizable", "List SectionTitle events")
+  /// Series
+  internal static let listSectionTitleSeries = L10n.tr("Localizable", "List SectionTitle series")
+  /// Marvel Universe
+  internal static let listTitle = L10n.tr("Localizable", "List Title")
   /// Loading
   internal static let loading = L10n.tr("Localizable", "Loading")
+  /// OK
+  internal static let messageViewDefaultAnswerButtonTitle = L10n.tr("Localizable", "MessageView DefaultAnswer ButtonTitle")
+  /// Please, change filters
+  internal static let messageViewEmptyCaseDescription = L10n.tr("Localizable", "MessageView EmptyCase Description")
+  /// No results
+  internal static let messageViewEmptyCaseTitle = L10n.tr("Localizable", "MessageView EmptyCase Title")
   /// Please, check your internet connection and try again
   internal static let messageViewErrorConnectionDescription = L10n.tr("Localizable", "MessageView ErrorConnection Description")
   /// Connection error
